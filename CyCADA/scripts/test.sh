a4a592f084000670a2910dc56a78ef33c7f39c08
tingzza <tingbaby112@gmail.com>
2025-04-19 17:11:29 +0800

    my cv assignment1 of DA,include sourceonly dann cyclegan cycada

---
+++ CyCADA/scripts/test.sh
@@ -0,0 +1,8 @@
+#------------------ Test ------------------
+phase="test"
+name="real_world"
+dataroot="data/art2world"
+python test.py \
+--name=${name} \
+--dataroot=${dataroot} \
+--phase=${phase} \
